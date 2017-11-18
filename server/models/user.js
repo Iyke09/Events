303cc4766d3d78bbfb662d25f71855e8a08e85e7
Emma Paris <iykay33@gmail.com>
2017-11-18 15:36:26 +0100

    ft:add user authentication using jwt

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,59 @@
 
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Oops. An account already exist with this username',
+        // fields: [sequelize.fn('lower', sequelize.col('email'))]
+      },
+      validate: {
+        len: {
+          args: [5, 10],
+          msg: 'Username must be btw 5 - 10 characters'
+        },
+        is: {
+          args: /^[A-Za-z][A-Za-z0-9-]+$/i,
+          msg: 'Username must start with a letter, have no spaces'
+        }
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Oops.The email you entered already exists',
+        // fields: [sequelize.fn('lower', sequelize.col('email'))]
+      },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'The email you entered is not valid',
+        },
+        max: {
+          args: 254,
+          msg: 'The email you entered is invalid or longer than 254 characters.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6],
+          msg: 'The password you entered is less than 3 characters'
+        },
+      },
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    }
+  });
+
+  return User;
+};
---
+++ server/routes/userRouter.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import userController from '../controllers/user/index';
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.status(200).send({
+  message: 'Welcome to the your Favorite API!',
+}));
+
+router.post('/signup', userController.signup);
+
+router.post('/signin', userController.signin);
+
+export default router;
